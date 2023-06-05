3a8c70f0e8210735c15f61b81f57d3c206c639e0
Pratiksha <shintre9pratiksha@gmail.com>
2023-06-05 17:34:48 +0530

    test

--- src/main/java/Hnadbags.java
+++ src/main/java/Hnadbags.java
@@ -1,5 +1,5 @@
 public class Hnadbags {
     public static void main (String args[]) {
-        System.out.println("Welcome to Shopping.com");
+        System.out.println("checkout handbags at shopping.com");
     }
 }
